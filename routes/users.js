0649b76a5f01a4aae4ac1b1c5ec1dcc99360c98d
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-07-01 10:21:30 -0400

    Add new route to /users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource')
+})
 
-module.exports = router;
+router.get('/cool', function (req, res, next) {
+  res.send(`You're so cool`)
+})
+
+module.exports = router
