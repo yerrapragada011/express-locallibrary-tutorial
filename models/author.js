8218ec2e55204ae1343c13468fbc12cf5340b8bb
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-07-08 13:44:34 -0400

    Add author list page & format death and birth dates

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const { DateTime } = require('luxon')
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
@@ -25,6 +26,18 @@ AuthorSchema.virtual('name').get(function () {
 AuthorSchema.virtual('url').get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`
+})
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : ''
+})
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : ''
 })
 
 // Export model
