0acc75578a5ea227b557391175f4f55b17dc9071
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-07-08 13:54:14 -0400

    Add genre list page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,7 +3,12 @@ const asyncHandler = require('express-async-handler')
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre list')
+  const allGenres = await Genre.find().exec()
+
+  res.render('genre_list', {
+    title: 'Genre List',
+    genre_list: allGenres
+  })
 })
 
 // Display detail page for a specific Genre.
